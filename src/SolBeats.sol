76dc9557266716132bbb93c36b10a665a4d34101
Colin Platt <colingplatt@gmail.com>
2023-06-15 17:30:58 +0200

    burn and validation

--- src/SolBeats.sol
+++ src/SolBeats.sol
@@ -8,14 +8,22 @@ interface IBEAT {
     function getBeat() external view returns (bytes memory);
 }
 
+interface UI {
+    function getUI() external view returns (string memory);
+}
+
 contract SolBeats is ERC721('SolBeats', unicode'🎚️'), Ownable {
 
     uint256 public constant MINT_COST = 0.001 ether;
 
     uint256 public totalSupply = 1;
+    UI public ui;
 
     mapping(uint256 => string) public beatStrings;
     mapping(bytes32 => bool) public beatMinted;
+
+    error UI_NOT_SET(); 
+    error ERC721_METADATA_URI_QUERY_FOR_NONEXISTENT_TOKEN(uint256 id);
 
     constructor() public {
         _initializeOwner(msg.sender);
@@ -23,13 +31,26 @@ contract SolBeats is ERC721('SolBeats', unicode'🎚️'), Ownable {
     }
 
     function tokenURI(uint256 id) public view override returns (string memory) {
-        return "S";
+        if(id == 0) {
+            if(address(ui) != address(0)) {
+                return ui.getUI();
+            } else {
+                revert UI_NOT_SET();
+            }
+        } else {
+            if(ownerOf(id) == address(0)) {
+                revert ERC721_METADATA_URI_QUERY_FOR_NONEXISTENT_TOKEN(id);
+            } else  {
+                (bytes memory buffer, uint32 sampleRate) = IBEAT(CREATE3.getDeployed(bytes32(id))).getBeat();
+                return WAV_8BIT.encodeWAV(buffer, sampleRate);
+            }
+        }
     }
 
     function mint(bytes calldata bytecode, string calldata beatString) public payable {
         require(msg.value == MINT_COST, "SolBeats: Mint cost is 0.001 ether");
 
-        bytes32 beatHash = keccak256(abi.encodePacked(bytecode));
+        bytes32 beatHash = keccak256(abi.encodePacked(beatString));
         require(!beatHash, "SolBeats: Beat already minted");
         beatMinted[beatHash] = true;
 
@@ -38,6 +59,19 @@ contract SolBeats is ERC721('SolBeats', unicode'🎚️'), Ownable {
         CREATE3.deploy(bytes32(tokenId), bytecode, 0);
         beatStrings[tokenId] = beatString;
         _mint(msg.sender, tokenId);
+    }
+
+    // if a user wants to change the offset or length on the same beat, they need to burn their existing token and mint a new one
+    function burn(uint256 id) public {
+        require(ownerOf(tokenId) == msg.sender, "SolBeats: Only owner can burn");
+        _burn(tokenId);
+
+        beatMinted[keccak256(abi.encodePacked(beatStrings[tokenId]))] = false;
+        beatStrings[tokenId] = "";
+    }
+
+    function setUI(address _newUI) public onlyOwner {
+        ui = UI(_newUI);
     }
 
 }
